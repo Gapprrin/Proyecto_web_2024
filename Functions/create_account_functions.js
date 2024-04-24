86aee3d9fe22978555aee937d26844de997c35db
unknown <Laboratorio@MPAPCA03LC0102.duoc.acad>
2024-04-24 16:54:42 -0400

    2.5.4

--- Functions/create_account_functions.js
+++ Functions/create_account_functions.js
@@ -30,13 +30,40 @@ function validarComuna(){
         "Lo Barnechea",
         "Lo Espejo"];
     
-    if(lista_comuna.includes(user.value) || user.value == "" || user.value == " "){
+    if(lista_comuna.includes(user.value.trim())){
         user.classList.add("valido");
         user.classList.remove("invalido");
         document.querySelector("#error-comuna").innerHTML = "";
+    }else if(user.value.trim() == ""){
+        user.classList.remove("invalido");
+        user.classList.remove("valido");
+        document.querySelector("#error-comuna").innerHTML = "Debe rellenar el campo.";
     }else{
         user.classList.add("invalido");
         user.classList.remove("valido");
         document.querySelector("#error-comuna").innerHTML = "No se ha encontrado la comuna.";
     }
+}
+
+
+function validarNombre(){
+    let user = document.querySelector("#input-nombre-usuario");
+
+    if(user.value.trim().length >= 5){
+        user.classList.add("valido");
+        user.classList.remove("invalido");
+        document.querySelector("#error-usuario").innerHTML = "";
+    }else if(user.value.trim() == ""){
+        user.classList.remove("invalido");
+        user.classList.remove("valido");
+        document.querySelector("#error-usuario").innerHTML = "Debe rellenar el campo.";
+    }else{
+        user.classList.add("invalido");
+        user.classList.remove("valido");
+        document.querySelector("#error-usuario").innerHTML = "Nombre de usuario inválido.";
+    }
+}
+
+function validarContrasenia(){
+    
 }
