ce5ae511e47077285ceefb852a0a254bc29bc5c8
Javiera55 <jav.mateluna@duocuc.cl>
2024-05-12 11:39:30 -0400

    testimonios

--- Functions/funciones.js
+++ Functions/funciones.js
@@ -76,6 +76,31 @@ getProductos();
 //APY USUARIOS---------------------------------------------------------------------------------------------------------------------------
 
 
+const boton = document.querySelector('#boton');
+const nombre = document.querySelector('#nombre');
+
+const foto = document.querySelector('#foto');
+
+const generarUsuario = async () => {
+    //consultar a la API
+    const url = 'https://randomuser.me/api/';
+    const respuesta = await fetch(url);
+    const { results } = await respuesta.json();
+    const datos = results[0];
+
+    foto.src = datos.picture.medium;
+    nombre.textContent = datos.name.first;
+    correo.textContent = datos.email;
+    telefono.textContent = datos.phone;
+}
+
+
+//crear un evento
+document.addEventListener('DOMContentLoaded', generarUsuario);
+boton.addEventListener('click', generarUsuario);
+
+
+
 
 
 
