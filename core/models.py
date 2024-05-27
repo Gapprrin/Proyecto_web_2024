d0a32265a5670608243552cb5c69c0b2391fc593
unknown <Laboratorio@MPAPCA03LC0102.duoc.acad>
2024-05-27 16:27:45 -0400

    avance bd

--- core/models.py
+++ core/models.py
@@ -5,7 +5,10 @@ class Producto(models.Model):
     id = models.CharField(max_length=4,primary_key=True)
     nombre = models.CharField(max_length=60)
     descripcion = models.CharField(max_length=200)
-    precio = models.IntegerField(max_length=10)
-    stock = models.IntegerField(max_length=10)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
     oferta = models.BooleanField()
     imagen = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nombre
--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    productos = Producto.objects.all()
+    return render(request, 'index.html', {'producto':productos})
 
 def login(request):
     return render(request, 'Pages/login.html')
