b432b8e7fbf7150c87af9a851d9c0b4eef527f52
Gapprrin <mate.alvarez@duocuc.cl>
2024-06-11 23:36:08 -0400

    Update 3.1.9

--- core/urls.py
+++ core/urls.py
@@ -6,11 +6,11 @@ from .views import *
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', home, name= "home"),
     path('modelos/', modelos, name= "modelos"),
     path('login/', LoginView.as_view(template_name="pages/login.html"), name= "login"),
     path('logon/', createAccount, name= "create_account"),
+    path('logout', logout, name="logout"),
     path('shopKart/', shopKart, name= 'Carrito'),
     path('accesorios/' , accesories , name= 'accesorios'),
     path('acerca_de/', AboutUs , name="acerca_de"),
